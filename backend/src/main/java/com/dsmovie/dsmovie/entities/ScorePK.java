c0b105d5eda8eb1d4c6f0448ab359830c6b7eb03
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-11 10:54:32 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/dsmovie/dsmovie/entities/ScorePK.java
@@ -0,0 +1,46 @@
+package com.dsmovie.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable { // a classe composta no JPA exige que importe o serializable
+	
+	private static final long serialVersionUID = 1L;
+
+
+	
+	// fazendo referência as chaves estrangeiras
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ScorePK() {
+		
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+}
