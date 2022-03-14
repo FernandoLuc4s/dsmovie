ae9068d5036aaaf9ed14cb69624da590102baeb7
FernandoLuc4s <fernandolucas2002@gmail.com>
2022-03-13 21:34:19 -0300

    removido import do moviescore para fazer deploy

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,4 @@
-import MovieScore from "components/MovieScore";
+
 import { Link } from "react-router-dom";
 
 function MovieCard() {
@@ -15,7 +15,7 @@ function MovieCard() {
         <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
         <div className="dsmovie-card-bottom-container">
             <h3>{movie.title}</h3>
-            <MovieScore />
+           
 
             <Link to={'/form/${movie.id}'}>
                 <div className="btn btn-primary dsmovie-btn">Avaliar</div>
